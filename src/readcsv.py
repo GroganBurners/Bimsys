8abff63e5711a802e403be77b702b3da5e7b1012
Neil Grogan <neil@grogan.ie>
2010-03-30 19:03:57 +0100

    Got structure of CSV -> DB, added back Service and Reapir CSV

--- src/readcsv.py
+++ src/readcsv.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 # Copyright Grogan Burner Services Ltd. 
 # Written by Neil Grogan
 # Licensed under GPL v3 or higher
 
+# Import Mark's database code
+import datastore
 import csv
 import cgi
 import os
@@ -42,17 +45,62 @@ def readInCSVFile():
 	f.close()
 	
 def processCSVtoDB():
+	#Start at 0 before Iteration
 	#headerno = 0
 	#colno = 0
-	#
-	#for row in column
-	#	header[headerno]
-	#	for row in column
-	#		print column[colno][headerno]
-	#		colno++
-	#	headerno++
-
 	
+	#Loop through all records, assigning correct values to DB fields
+	#for row in column
+		## Row 0 is header, so throw away
+		#if rownum == 0: 
+			##discard
+		#elif rownum >= 1:
+			## Test if the customer ID is unique, not in use (yet)
+			##if (custid != row[0])
+			#cust_id = column[rownum][0]
+			#cust_type
+			#cust_date
+			## TODO Split strings after space
+			#cust_first_name =
+			#cust_last_name = 
+			## END TODO
+			#cust_password =
+			#cust_address1 =
+			#cust_address2 =
+			#cust_address3 =
+			#cust_county =
+			#cust_geocode = '0.0,0.0'
+			#cust_phone =
+			#cust_mobile_phone =
+			
+			###########
+			## Services
+			###########
+			#type_of_work = #Service/Install/Repair/Other
+			#next_service = #Date of next service due
+    			#notes = 
+    			
+			#####################
+			## Service Statistics
+			#####################
+			#o2 = 
+    			#coppm = 
+    			#co2percent = 
+    			#flumeTemp = 
+    			#efficiency = 
+    			#xsair = 		 
+			## Debug printout
+			#print column[colno][headerno]
+	
+			#colno++
+		#headerno++
+
+#####################
+## Export DB to CSV
+####################
+def processCSVtoDB():
+
+
 
 if __name__ == "__main__":
     readInCSVFile()

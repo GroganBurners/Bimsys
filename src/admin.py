4336f77b2725a1ce5cd6a921e0e310333e461253
Neil Grogan <neil@grogan.ie>
2010-03-30 18:26:38 +0100

    Added uncommitted things, js, html, templates etc

---
+++ src/admin.py
@@ -0,0 +1,35 @@
+#Imports Start
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+#Import DataStore modules
+import datastore
+#Import DataStore modules End
+
+#Imports End
+count = 0
+customerList = datastore.Customer.all()
+
+for i in customerList:
+    count = count + 1
+
+results = customerList.fetch(count)
+
+
+class Admin(webapp.RequestHandler):
+    def get(self):
+        #Request the current size of the datastore module
+        #storeSize = datastore.Customer.__sizeof__()
+        #Assign the whole store to this variable
+        #Get the whole datastore
+        #template.render(path,{'results':results})
+        path = os.path.join(os.path.dirname(__file__)+'/templates/admin/','base1Column.html')
+        self.response.out.write(template.render(path,{'results':results}))
+        
+class oilService(webapp.RequestHandler):
+    def get(self):
+        customerList.filter(type, '1')
+        path = os.path.join(os.path.dirname(__file__)+'/templates/admin/','base1Column.html')
+        self.response.out.write(template.render(path,{'results':results}))
+        
---
+++ src/datastore.py
@@ -0,0 +1,32 @@
+from google.appengine.ext import db
+
+class Customer (db.Model):
+    cust_id = db.IntegerProperty()#Unique ID
+    cust_type = db.StringProperty()#Generated from a dropdown menu Gas service/Gas Repair/Oil Service/Oil Repair
+    cust_date = db.DateProperty(auto_now_add=True)#if not provided automatically generated
+    cust_first_name = db.StringProperty()
+    cust_last_name = db.StringProperty()
+    cust_email = db.StringProperty()
+    cust_password = db.StringProperty()
+    cust_address1 = db.StringProperty()
+    cust_address2 = db.StringProperty()
+    cust_address3 = db.StringProperty()
+    cust_county = db.StringProperty()
+    cust_geocode = db.StringProperty(default = '0.0,0.0')#automatically generated if available
+    cust_phone = db.PhoneNumberProperty()
+    cust_mobile_phone = db.PhoneNumberProperty()
+    
+    type_of_work = db.StringProperty() #Service/Install/Repair/Other
+    next_service = db.DateProperty() #Date of next service due
+    notes = db.StringProperty()
+    
+    o2 = db.FloatProperty(0)
+    coppm = db.FloatProperty(0)
+    co2percent = db.FloatProperty(0)
+    flumeTemp = db.FloatProperty(0)
+    efficiency = db.FloatProperty(0)
+    xsair = db.FloatProperty(0)
+    
+class pageNews(db.Model):
+    news_heading = db.StringProperty
+    news_body = db.StringProperty
